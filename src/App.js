d3b3a65246cdaa024407e394061d776d016148d7
Lara <pedroso5@hotmail.com>
2022-01-26 15:37:59 -0300

    React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+  import React, { Component }from 'react';
+  import Header from './components/header/Header';
+  import Tasks from './components/tasks/Tasks';
+  import Container from 'react-bootstrap/Container'
+  import { library } from '@fortawesome/fontawesome-svg-core'
+  import { faCheckCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+  import './App.scss';
+  library.add(faCheckCircle, faTrashAlt)
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Container>
+        <Tasks/>
+      </Container>
     </div>
   );
 }
 
-export default App;
